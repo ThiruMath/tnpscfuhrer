73460d6d10585cd4c6f87c559abf24bacf2b855e
Thirumurugan <thirumuruganwork@gmail.com>
2026-03-11 07:28:16 +0530

    fix: use static output, remove cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import cloudflare from '@astrojs/cloudflare';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://tnpscfuhrer.in',
   integrations: [mdx(), sitemap()],
-  adapter: cloudflare(),
+  output: 'static',
 });
